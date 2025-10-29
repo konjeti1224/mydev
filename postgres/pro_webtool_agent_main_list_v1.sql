b656eb3fc2c16752c3df7055bf1ad7abbc577dd2
Vijay <sekhar1224@outlook.com>
2025-10-29 15:13:09 +0530

    29102025
    29102025

--- postgres/pro_webtool_agent_main_list_v1.sql
+++ postgres/pro_webtool_agent_main_list_v1.sql
@@ -13,74 +13,104 @@ DECLARE
     lv_json jsonb;
 BEGIN
     SELECT jsonb_build_object(
-        'statusWiseCount', jsonb_build_object(
-            'booked', COUNT(*) FILTER (WHERE wslv.status IN ('Booked', 'Received')),
-            'arrived', COUNT(*) FILTER (WHERE wslv.status = 'Arrived'),
-            'inTransit', COUNT(*) FILTER (WHERE wslv.status = 'In Transit'),
-            'cancelled', COUNT(*) FILTER (WHERE wslv.status = 'Cancelled'),
-            'delivered', COUNT(*) FILTER (WHERE wslv.status = 'Delivered'),
-            'all', COUNT(*)
+    'statusWiseCount', jsonb_build_object(
+        'self', COUNT(*) FILTER (WHERE t.roleType = 'SELF'),
+        'nominated', COUNT(*) FILTER (WHERE t.roleType = 'NOMINATED'),
+        'all', COUNT(*)
+    ),
+    'shipment', COALESCE(
+        jsonb_agg(
+            jsonb_build_object(
+                'id', t.id,
+                'shipment', t.shipment_uid,
+                'bookedDate', t.shipment_date,
+                'hblNumber', t.hbl_no,
+                'origin', t.origin_name,
+                'originCountryCode', t.origin_country_code,
+                'destination', t.destination_name,
+                'destinationCountryCode', t.destination_country_code,
+                'mode', t.transport_mode,
+                'orderNo', t.order_no,
+                'tosName', t.tos_name,
+                'bookingDate', t.order_confimration,
+                'readyDate', t.order_ready,
+                'pickupDate', t.order_pickup,
+                'departure', t.order_departure,
+                'arrival', t.order_arrival,
+                'estimatedDeparture', to_char(t.etd, 'DD-MON-YY'),
+                'actualDeparture', to_char(t.atd, 'DD-MON-YY'),
+                'estimatedArrival', to_char(t.eta, 'DD-MON-YY'),
+                'actualArrival', to_char(t.ata, 'DD-MON-YY'),
+                'status', t.status,
+                'roleType', t.roleType
+            )
         ),
-        'shipment', COALESCE(
-            jsonb_agg(
-                jsonb_build_object(
-                    'id', wslv.id,
-                    'shipment', wslv.shipment_uid,
-                    'bookedDate', wslv.shipment_date,
-                    'hblNumber', wslv.hbl_no,
-                    'origin', wslv.origin_name,
-                    'originCountryCode', wslv.origin_country_code,
-                    'destination', wslv.destination_name,
-                    'destinationCountryCode', wslv.destination_country_code,
-                    'mode', wslv.transport_mode,
-                    'orderNo', wslv.order_no,
-                    'tosName', wslv.tos_name,
-                    'bookingDate', wslv.order_confimration,
-                    'readyDate', wslv.order_ready,
-                    'pickupDate', wslv.order_pickup,
-                    'departure', wslv.order_departure,
-                    'arrival', wslv.order_arrival,
-                    'estimatedDeparture', to_char(wslv.etd, 'DD-MON-YY'),
-                    'actualDeparture', to_char(wslv.atd, 'DD-MON-YY'),
-                    'estimatedArrival', to_char(wslv.eta, 'DD-MON-YY'),
-                    'actualArrival', to_char(wslv.ata, 'DD-MON-YY'),
-                    'status', wslv.status,
-                    'roleType',
-                        CASE 
-                            -- ADMIN logic
-                            WHEN fa_role = 'ADMIN'
-                                 AND wslv.routed = 'SELF'
-                                 AND (party.origin_agent_id = wud.nxt_customer_id or party.destination_agent_id = wud.nxt_customer_id)
-                            THEN 'SELF'
-                            WHEN fa_role = 'ADMIN'
-                                 AND wslv.routed = 'AGENT'
-                                 AND wslv.routed_by_id = wud.nxt_customer_id
-                            THEN 'NOMINATED'
-                            
-                            -- AGENT logic
-                            WHEN fa_role = 'AGENT'
-                                 AND wslv.routed = 'SELF'
-                                  AND (party.origin_agent_id = wud.nxt_customer_id or party.destination_agent_id = wud.nxt_customer_id)
-                            THEN 'NOMINATED'
-                            WHEN fa_role = 'AGENT'
-                                 AND wslv.routed = 'AGENT'
-                                 AND wslv.routed_by_id = wud.nxt_customer_id
-                            THEN 'SELF'
-
-                            ELSE NULL
-                        END
-                )
-            ),
-            '[]'::jsonb
-        )
+        '[]'::jsonb
     )
-    INTO lv_json
+) into lv_json
+FROM (
+    SELECT 
+        wslv.id,
+        wslv.shipment_uid,
+        wslv.shipment_date,
+        wslv.hbl_no,
+        wslv.origin_name,
+        wslv.origin_country_code,
+        wslv.destination_name,
+        wslv.destination_country_code,
+        wslv.transport_mode,
+        wslv.order_no,
+        wslv.tos_name,
+        wslv.order_confimration,
+        wslv.order_ready,
+        wslv.order_pickup,
+        wslv.order_departure,
+        wslv.order_arrival,
+        wslv.etd,
+        wslv.atd,
+        wslv.eta,
+        wslv.ata,
+        wslv.status,
+        wslv.routed,
+        wslv.routed_by_id,
+        party.origin_agent_id,
+        party.destination_agent_id,
+        wud.nxt_customer_id,
+        wum.id AS user_id,
+        CASE 
+            -- ADMIN logic
+            WHEN fa_role = 'ADMIN'
+                 AND wslv.routed = 'Self'
+                 AND (party.origin_agent_id = wud.nxt_customer_id OR party.destination_agent_id = wud.nxt_customer_id)
+            THEN 'SELF'
+            WHEN fa_role = 'ADMIN'
+                 AND wslv.routed = 'Agent'
+                 AND wslv.routed_by_id = wud.nxt_customer_id
+            THEN 'NOMINATED'
+            
+            -- AGENT logic
+            WHEN fa_role = 'AGENT'
+                 AND wslv.routed = 'Self'
+                 AND (party.origin_agent_id = wud.nxt_customer_id OR party.destination_agent_id = wud.nxt_customer_id)
+            THEN 'NOMINATED'
+            WHEN fa_role = 'AGENT'
+                 AND wslv.routed = 'Agent'
+                 AND wslv.routed_by_id = wud.nxt_customer_id
+            THEN 'SELF'
+            ELSE NULL
+        END AS roleType
     FROM webtool_shipment_list_view wslv
-	 LEFT JOIN shipment_party_detail party ON wslv.id = party.shipment_header_id
-    JOIN web_user_detail wud ON (party.origin_agent_id = wud.nxt_customer_id or party.destination_agent_id = wud.nxt_customer_id or  wslv.routed_by_id = wud.nxt_customer_id)
-    JOIN web_user_master wum ON wum.id = wud.registration_no   
+    LEFT JOIN shipment_party_detail party 
+        ON wslv.id = party.shipment_header_id
+    JOIN web_user_detail wud 
+        ON (party.origin_agent_id = wud.nxt_customer_id 
+            OR party.destination_agent_id = wud.nxt_customer_id 
+            OR wslv.routed_by_id = wud.nxt_customer_id)
+    JOIN web_user_master wum 
+        ON wum.id = wud.registration_no
     WHERE wum.id = fa_login_id
-      AND wslv.shipment_date >= CURRENT_DATE - (fa_date_filter_days || ' days')::interval;
+      AND wslv.shipment_date >= CURRENT_DATE - (fa_date_filter_days || ' days')::interval
+) t;
 
     -- Return result
     IF lv_json IS NULL THEN
